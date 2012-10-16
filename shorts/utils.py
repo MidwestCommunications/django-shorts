4937a2780b52b8647d6da65593421934ab180e50
Jake Alstad <jake.alstad@gmail.com>
2012-10-16 10:23:55 -0500

    findURL function returns shortened url, ready to integrate

--- shorts/utils.py
+++ shorts/utils.py
@@ -1,3 +1,5 @@
+import re
+
 from bitly_api import bitly_api
 
 from django.contrib.sites.models import Site
@@ -5,20 +7,29 @@ from django.contrib.sites.models import Site
 from shorts.models import ShortURL
 
 
-def shorten_url(site, url):
-    bitly = bitly_api.Connection('midwestcommunications', 'R_8cf0d869efb892d4f8bbfe3601fa84c2')
-    bitly_url = bitly.shorten(url)
+def findURL(text):
+    #Run the text through a regex to find all urls.
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
 
-    if not ShortURL.objects.filter(shortened_url=bitly_url['url']):
+    #Loop through list of found urls and shorten_url on each.
+    if urls:
+        for url in urls:
+            return shorten_url(Site.objects.get_current(), url).shortened_url
+
+    return 
+
+def shorten_url(site, url):
+    #Connect to bit.ly api and call bit.ly shorten method on passed in url.
+    bitly = bitly_api.Connection('midwestcommunications', 'R_8cf0d869efb892d4f8bbfe3601fa84c2')
+
+    #make sure url isn't already stored locally, shorten it, and save to database.
+    if not ShortURL.objects.filter(original_url=url):
+        bitly_url = bitly.shorten(url)
         shortened_url = ShortURL(site=site, original_url=url, shortened_url=bitly_url['url'])
         shortened_url.save()
+    else:
+        shortened_url = ShortURL.objects.get(original_url=url)
 
-    return
+        return shortened_url
 
-def expand_url(site, url):
-    bitly = bitly_api.Connection('midwestcommunications', 'R_8cf0d869efb892d4f8bbfe3601fa84c2')
-    bitly_url = bitly.expand(shortUrl=url)
-
-    original_url = bitly_url[0]['long_url']
-
-    return original_url
+    return shortened_url
